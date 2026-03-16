643b727c473f8b0f4fb873c2cac4159461cc1c9c
jatin0670 <jp0670786@gmail.com>
2026-03-16 11:30:32 +0530

    files

--- Frontend/libraryManagement/src/components/ManageBook.jsx
+++ Frontend/libraryManagement/src/components/ManageBook.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 
-// add local host 8080 to run locally
+// add this - http://localhost:8080   to run locally
 
 const ManageBook = () => {  
 
